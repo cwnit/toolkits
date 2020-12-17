6f4a6908bd9d7b55117c20736cf9214c323c4507
jpgomez-at-512175513160 <33204771+jpgomez-cwn@users.noreply.github.com>
2020-12-16 21:51:01 -0500

    minor correction to uploade file

---
+++ collections/powershell/aws/s3objectupload.ps1
@@ -0,0 +1,44 @@
+#CLEAR DEFAULT CREDENTIALS
+Clear-AWSCredential
+
+#INTIALIZE AWS API
+Initialize-AWSDefaults
+
+#FORCE TLS 1.2 CONNECTION TO API
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+
+# Your account access key - must have read access to your S3 Bucket
+$accessKey = Read-Host "Enter AWS Accesskey" -AsSecureString
+
+# Your account secret access key
+$secretKey = Read-Host "Enter AWS Secret" -AsSecureString
+
+# The region associated with your bucket e.g. eu-west-1, us-east-1 etc. (see http://docs.aws.amazon.com/AWSEC2/latest/UserGuide/using-regions-availability-zones.html#concepts-regions)
+$region = "us-east-1"
+
+# The name of your S3 Bucket
+$bucket = 'mys3bucket'
+
+#s3Folderlocation
+$keylocation = 'foldername'
+
+#The name of the file you are dowloading
+$file = 'example.msi'
+
+#current User logged in
+$username = $env:UserName
+
+#Local File path
+$filepath = "C:\Users\$($username)\Downloads"
+
+#DECRYPT VARIABLES IN MEMORY SECRET KEY
+$accessKey = [Runtime.InteropServices.Marshal]::SecureStringToBSTR($accessKey)
+$accessKey = [Runtime.InteropServices.Marshal]::PtrToStringBSTR($accessKey)
+
+#DECRYPT VARIABLES IN MEMORY SECRET KEY
+$secretKey = [Runtime.InteropServices.Marshal]::SecureStringToBSTR($secretKey)
+$secretKey = [Runtime.InteropServices.Marshal]::PtrToStringBSTR($secretKey)
+
+#EXECUTE COMMAND THAT DOWNLOADS CONTENT TO DEFAULT DOWNLOAD DIRECTORY
+
+Write-S3Object -BucketName $bucket -File "$($filepath)\$($file)" -Key "$($keylocation)/$($file)" -AccessKey $accesskey -SecretKey $secretKey
