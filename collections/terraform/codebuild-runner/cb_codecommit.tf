049317cd2d257d49bb0b483c45b305f325978587
JPGOMEZ <33204771+jpgomez-cwn@users.noreply.github.com>
2022-06-22 20:50:31 -0400

    Provided sample of single Runner

--- collections/terraform/codebuild-runner/cb_codecommit.tf
+++ collections/terraform/codebuild-runner/cb_codecommit.tf
@@ -1,1 +1,28 @@
-coming soon
+resource "aws_codebuild_project" "codebuild_project_test" {
+    name          = "${var.myname}-cb-runner"
+    description   = var.description
+    build_timeout = var.build_timeout
+    service_role  = var.cb_service_role
+
+    artifacts {
+        type = "NO_ARTIFACTS"
+    }
+
+    environment {
+        compute_type                = "BUILD_GENERAL1_SMALL"
+        image                       = "aws/codebuild/standard:2.0"
+        type                        = "LINUX_CONTAINER"
+        image_pull_credentials_type = "CODEBUILD"
+        privileged_mode             = true
+
+    }
+
+    source {
+        type              = "CODECOMMIT"
+        buildspec         = "buildspec.yml"
+        git_clone_depth   = 0
+        location          = https://git-codecommit.us-east-1.amazonaws.com/v1/repos/[repo_name]" #MUST BE THE HTTPS URL!!
+        
+    }
+
+}
