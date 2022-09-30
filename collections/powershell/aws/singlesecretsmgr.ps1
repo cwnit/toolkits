d3d272f964f3a2d11a14851b6e29c324d2200795
juanpgomez-gsa <juan.gomez@gsa.gov>
2022-09-29 23:27:40 -0400

    Added new script for secret manager value.

---
+++ collections/powershell/aws/singlesecretsmgr.ps1
@@ -0,0 +1,41 @@
+<#---------------------------------------------------------------------------------------------------
+SCRIPT NAME:  GET SINGLE SECRET FROM AWS SECRETS MANAGER
+DESCRIPTION:  Run this script as a scheduled task that runs daily to update admin password
+---------------------------------------------------------------------------------------------------#>
+
+
+$sm_value = "Name of your secret"
+
+$aws_secret = Get-SECSecretValue -SecretId $sm_value
+
+$mysecret = $aws_secret.SecretString
+
+$newsecuresecret = ConvertTo-SecureString -String $mysecret -AsPlainText -Force
+
+
+<#
+Example AWS Policy
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Action": [
+                "secretsmanager:GetResourcePolicy",
+                "secretsmanager:GetSecretValue",
+                "secretsmanager:DescribeSecret",
+                "secretsmanager:ListSecretVersionIds"
+            ],
+            "Resource": [
+                "arn:aws:secretsmanager:us-east-1:111122223333:secret:Name of your secret"
+            ]
+        },
+        {
+            "Effect": "Allow",
+            "Action": "secretsmanager:ListSecrets",
+            "Resource": "*"
+        }
+    ]
+}
+
+#>
