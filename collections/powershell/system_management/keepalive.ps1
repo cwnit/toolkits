b61132019149830381f9ddd91c4c1be08c4febaa
JPGOMEZ <33204771+jpgomez-cwn@users.noreply.github.com>
2026-01-05 12:36:07 -0500

    Update keepalive.ps1
    update capabilities of the script

--- collections/powershell/system_management/keepalive.ps1
+++ collections/powershell/system_management/keepalive.ps1
@@ -1,19 +1,23 @@
-Clear-Host
-
-Echo "Keep alive w/ scroll lock..."
-
-$WShell = New-Object -com "Wscript.Shell"
-
-while ($true)
-
+#CREATE DIRECTORY WHERE INSTALLATION FILES EXISTS
+$path = "C:\Utilities"
+If(!(test-path -PathType container $path))
 {
+      New-Item -ItemType Directory -Path $path
+}
 
-  $WShell.sendkeys("{SCROLLLOCK}")
+$folders = "bigfix", "fireeye", "Carbon_Black_Protection", "tenable-nessusagent", "secureconnector", "server_certificates", "winlogbeat", "solarWinds-agent", "endgame", "powershell"
 
-  Start-Sleep -Milliseconds 100
+#BUILD Local Folders
+foreach ($folder in $folders) {
 
-  $WShell.sendkeys("{SCROLLLOCK}")
+    $folderkey = "utilities/packages/windows/$folder"
+    $folderpath = "C:\utilities\packages\$folder"
 
-  Start-Sleep -Seconds 240
+    echo "Folde key: $folderkey"
+
+    Read-S3Object -BucketName "iha-test-mgt-ami-packages-s3" -KeyPrefix $folderkey -folder $folderpath  
+
+    echo "s3 downloaded"
+
 
 }
